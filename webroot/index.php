6c25f5df1353643089f441112ef0fb4f4103ffcd
Patrick Thompson <patrick.thompson901@gmail.com>
2019-01-09 12:02:17 +0000

    styling navbar and index.php

--- webroot/index.php
+++ webroot/index.php
@@ -25,6 +25,33 @@
     <h3>Welcome to MiTeck Tracking System dashboard <?php echo $_SESSION['username']; ?></h3>
     <p>Find out everything you need to know about the status of each individual product</p>
   </div>
+  <div class="wrapper dashboard">
+      <div class="box col-1-1fr">
+          <div class="notifications-pane">
+              <h1>Notifications</h1>
+              <hr />
+          </div>
+      </div>
+      <br>
+      <div class="box col-2-1fr">
+          <div class="dashboard-pane">
+              <h1>Latest Materials Jobs</h1>
+              <hr />
+          </div>
+          <div class="dashboard-pane">
+              <h1>Latest Cutting Jobs</h1>
+              <hr />
+          </div>
+          <div class="dashboard-pane">
+              <h1>Latest Making Jobs</h1>
+              <hr />
+          </div>
+          <div class="dashboard-pane">
+              <h1>Latest Loading Jobs</h1>
+              <hr />
+          </div>
+      </div>
+  </div>
   <?php require($DOCUMENT_ROOT . "includes/footer.php"); ?>
 </body>
 </html>
