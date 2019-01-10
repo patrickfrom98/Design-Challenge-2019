c33186fe7ae20ca279b850bf1f6f88aedd4e86a2
J.O.Stocks U1757754 <U1757754@unimail.hud.ac.uk>
2019-01-10 11:20:02 +0000

    Index process script

---
+++ webroot/index-process.php
@@ -0,0 +1,34 @@
+<?php
+  session_start();
+
+  //Create database connection
+  function getConnection(){
+    try {
+        $pdo = new PDO("mysql:host=localhost; dbname=team24", 'team24', 'password');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+  }
+
+  function getTrussStates($stateType){
+    $pdo = getConnection();
+    $statesQuery = "SELECT * FROM truss
+      INNER JOIN truss_state ON truss.truss_id = truss_state.truss_id
+      INNER JOIN state ON truss_state.state_id = state.state_id
+      INNER JOIN state_type ON state.state_type_id = state_type.state_type_id
+      WHERE state_type.state_name = :stateType
+      ORDER BY state.timestamp DESC
+      Limit 10;";
+    $prepType = $pdo->prepare($statesQuery);
+    $prepType->bindValue(':stateType', $stateType);
+    $prepType->execute();
+    $states = $prepType->fetchAll();
+    return $states;
+  }
+
+  $materialsStates = getTrussStates("Pick Materials");
+  print_r($materialsStates);
+
+?>
