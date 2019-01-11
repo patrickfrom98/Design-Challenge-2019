6e922caa4ddd8921ab30c7e206a575199da3001a
J-Stocks <36339338+J-Stocks@users.noreply.github.com>
2019-01-11 01:32:30 +0000

    Changed to better use $_GET

--- webroot/sensor_process.php
+++ webroot/sensor_process.php
@@ -1,14 +1,15 @@
 <?php
+// print_r($_GET);
   if (empty($_GET)) {
 ?>
-<form action="sensor_process.php" method="post" id="sensorSubmit">
+<!-- <form action="sensor_process.php" method="get" id="sensorSubmit">
   <input type="file" id="json" name="json">
   <input type="submit" id="submit" name="submit" value="submit">
-</form>
+</form> -->
 
 <?php
   } else {
-    $jsonRaw = $_GET["json"];
+    // $jsonRaw = $_GET["json"];
     // $jsonRaw = '{
     //   "truss_id":"1",
     //   "sensor_id":"1",
@@ -16,40 +17,40 @@
     //   "timestamp":"2019-02-21 09:28:00",
     //   "location":"Warehouse 1"
     // }';
-    $jsonArray = explode(',', $jsonRaw);
-    $cleanArray = [];
-    foreach ($jsonArray as $jsonValue) {
-      $charArray = str_split($jsonValue);
-      $filteredArray = array_filter($charArray, function($char){
-        if (in_array($char, ['{', '}', ',' ,'"', "'"])) {
-          return false;
-        }
-        return true;
-      });
-      if (!empty($filteredArray)) {
-        array_push($cleanArray, implode($filteredArray));
-      }
-    }
-
-    $truss_id = substr($cleanArray[0], (strpos($cleanArray[0], ':') + 1));
-    $sensor_id = substr($cleanArray[1], (strpos($cleanArray[1], ':') + 1));
-    $state_type_id = substr($cleanArray[2], (strpos($cleanArray[2], ':') + 1));
-    $timestamp = substr($cleanArray[3], (strpos($cleanArray[3], ':') + 1));
-    $location = substr($cleanArray[4], (strpos($cleanArray[4], ':') + 1));
+    // $jsonArray = explode(',', $jsonRaw);
+    // $cleanArray = [];
+    // foreach ($jsonArray as $jsonValue) {
+    //   $charArray = str_split($jsonValue);
+    //   $filteredArray = array_filter($charArray, function($char){
+    //     if (in_array($char, ['{', '}', ',' ,'"', "'"])) {
+    //       return false;
+    //     }
+    //     return true;
+    //   });
+    //   if (!empty($filteredArray)) {
+    //     array_push($cleanArray, implode($filteredArray));
+    //   }
+    // }
+    //
+    // $truss_id = substr($cleanArray[0], (strpos($cleanArray[0], ':') + 1));
+    // $sensor_id = substr($cleanArray[1], (strpos($cleanArray[1], ':') + 1));
+    // $state_type_id = substr($cleanArray[2], (strpos($cleanArray[2], ':') + 1));
+    // $timestamp = substr($cleanArray[3], (strpos($cleanArray[3], ':') + 1));
+    // $location = substr($cleanArray[4], (strpos($cleanArray[4], ':') + 1));
 
     try {
-        $pdo = new PDO("mysql:host=localhost; dbname=u1756102", 'u1756102', '08nov98');
+        $pdo = new PDO("mysql:host=localhost; dbname=team24", 'team24', 'password');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
 
-    $stateCreationQuery = "INSERT INTO state (state_id, state_type_id, `timestamp`, location, data) VALUES (NULL, :state_type_id, :timest, :location, :jsonRaw);";
+    $stateCreationQuery = "INSERT INTO state (state_id, state_type_id, `timestamp`, location, data) VALUES (NULL, :state_type_id, :timest, :location, NULL);";
     $prepState = $pdo->prepare($stateCreationQuery);
-    $prepState->bindValue(':state_type_id', $state_type_id);
-    $prepState->bindValue(':timest', $timestamp);
-    $prepState->bindValue(':location', $location);
-    $prepState->bindValue(':jsonRaw', $jsonRaw);
+    $prepState->bindValue(':state_type_id', $_GET["state_type_id"]);
+    $prepState->bindValue(':timest', $_GET["timestamp"]);
+    $prepState->bindValue(':location', $_GET["location"]);
+    // $prepState->bindValue(':jsonRaw', $_GET["state_type_id"]);
     $prepState->execute();
 
     $stateVerifyQuery = "SELECT state_id FROM state ORDER BY state_id DESC LIMIT 1;";
@@ -58,13 +59,13 @@
 
     $trussStateCreationQuery = "INSERT INTO truss_state (truss_id, state_id) VALUES (:truss_id, :state_id);";
     $prepTrussState = $pdo->prepare($trussStateCreationQuery);
-    $prepTrussState->bindValue(':truss_id', $truss_id);
+    $prepTrussState->bindValue(':truss_id', $_GET["truss_id"]);
     $prepTrussState->bindValue(':state_id', $state_id);
     $prepTrussState->execute();
 
     $sensorStateCreationQuery = "INSERT INTO sensor_state (sensor_id, state_id) VALUES (:sensor_id, :state_id);";
     $prepSensorState = $pdo->prepare($sensorStateCreationQuery);
-    $prepSensorState->bindValue(':sensor_id', $sensor_id);
+    $prepSensorState->bindValue(':sensor_id', $_GET["sensor_id"]);
     $prepSensorState->bindValue(':state_id', $state_id);
     $prepSensorState->execute();
   }
