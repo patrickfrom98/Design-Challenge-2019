78371db6598a595367ba9f9e84959b0e7162f0ff
Patrick Thompson <32361791+patrickfrom98@users.noreply.github.com>
2019-04-06 17:39:04 +0100

    Update login_process.php

--- webroot/login_process.php
+++ webroot/login_process.php
@@ -10,7 +10,7 @@
 
       //Create database connection
       try {
-          $pdo = new PDO("mysql:host=localhost; dbname=u1756102", 'u1756102', '08nov98');
+          $pdo = new PDO("mysql:host=localhost; dbname=", '', '');
           $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
           echo "Connection failed: " . $e->getMessage();
